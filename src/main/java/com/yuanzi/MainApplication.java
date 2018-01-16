53156bee7dfd17f6fc339931bca397d6866b3765
xueyya <xueyya@seentao.com>
2018-01-16 09:30:59 +0800

    修改代码
    Signed-off-by: xueyya <xueyya@seentao.com>

--- src/main/java/com/yuanzi/MainApplication.java
+++ src/main/java/com/yuanzi/MainApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 //@EnableFeignClients
 //@EnableCircuitBreaker
 @RestController
